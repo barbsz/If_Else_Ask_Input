619b6c59b3f59d37ebe8adab7eb27d56e116a019
John Darael Valdez Degano <91790416+barbsz@users.noreply.github.com>
2024-01-08 03:10:54 +0700

    Psuedo code

---
+++ If_Else_Ask_Input.py
@@ -0,0 +1,13 @@
+#Assignment 4
+
+#Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+#Psuedo Code
+
+#Ask the user to input 3 numbers
+#Ask to input number 1
+#Ask to input number 2
+#Ask to input number 3 
+#Check if the input are numbers
+#If the input are numbers find and print the biggest number
+#If the input is not a number print "please input a number"
